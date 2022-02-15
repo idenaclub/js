f8f67b4f55fd2860e20a58382e87c42049d2972b
idenaclub <75692678+idenaclub@users.noreply.github.com>
2022-02-16 00:15:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,16 +31,3 @@ async function sendTransaction() {
     })
     
 }
-/////
-const ethereumButton = document.querySelector('.enableEthereumButton');
-const showAccount = document.querySelector('.showAccount');
-
-ethereumButton.addEventListener('click', () => {
-  getAccount();
-});
-
-async function getAccount() {
-  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-  const account = accounts[0];
-  showAccount.innerHTML = account;
-}
